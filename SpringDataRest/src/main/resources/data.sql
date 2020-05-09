c25a414ab6b243b3f6b091f9c53731b0ef8b91b4
Axel Kouassi <ak659@njit.edu>
2020-05-09 15:55:51 -0400

    Spring Data Rest

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(104,'Axel', 'Java');
+insert into alien values(105,'Geoffroy', 'C#');
+insert into alien values(106,'Kouassi', 'C++');
+insert into alien values(107,'Landry', 'Python');
+insert into alien values(108,'Oswaldt', 'SQL');
